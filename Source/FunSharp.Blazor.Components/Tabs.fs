7df30f77d78b3273bb41e697e7a1ee398a549190
Malbios <no@mail.none>
2025-09-10 14:02:53 +0200

    - added Tabs component - UI improvements

---
+++ Source/FunSharp.Blazor.Components/Tabs.fs
@@ -0,0 +1,29 @@
+﻿namespace FunSharp.Blazor.Components
+
+open Bolero
+open Bolero.Html
+open Radzen.Blazor
+
+[<RequireQualifiedAccess>]
+module Tabs =
+    
+    type Item = {
+        Label: string
+        RenderAction: unit -> Node
+    }
+    
+    let render (items: Item array) =
+        
+        let tabs =
+            [|
+                for item in items do
+                    yield comp<RadzenTabsItem> {
+                        "Text" => item.Label
+                        attr.fragment "ChildContent" (item.RenderAction ())
+                    }
+            |]
+            |> Helpers.renderArray
+        
+        comp<RadzenTabs> {
+            attr.fragment "Tabs" tabs
+        }
