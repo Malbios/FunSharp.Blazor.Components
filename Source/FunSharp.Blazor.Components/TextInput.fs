844b2f92e4cb25dc6dca44ed94a1abb95b610ede
Malbios <no@mail.none>
2025-09-11 16:04:50 +0200

    small stuff

--- Source/FunSharp.Blazor.Components/TextInput.fs
+++ Source/FunSharp.Blazor.Components/TextInput.fs
@@ -1,22 +1,32 @@
 ﻿namespace FunSharp.Blazor.Components
 
 open Bolero.Html
+open Microsoft.AspNetCore.Components
+open Microsoft.AspNetCore.Components.Web
 open Radzen.Blazor
 
 [<RequireQualifiedAccess>]
-module TextInput =
+module TextInput = // TODO: turn into Component because of statefulness
     
-    let render (update: string -> unit) placeholder currentValue =
+    let render (update: string -> unit) (onEnter: string -> unit) placeholder currentValue =
+        
+        let mutable savedValue = currentValue
         
         let update (newValue: string) =
-            if currentValue <> newValue then
+            if newValue <> currentValue then
                 update newValue
+                
+        let onKeyUp (args: KeyboardEventArgs) =
+            if args.Key = "Enter" then
+                onEnter savedValue
+                
+        let onChange (args: ChangeEventArgs) =
+            savedValue <- args.Value.ToString()
         
         comp<RadzenTextBox> {
             attr.callback "Change" update
-            
-            // TODO: maybe needs this, maybe not
-            // attr.callback "oninput" (fun (args: ChangeEventArgs) -> args.Value.ToString() |> update)
+            attr.callback "onkeyup" onKeyUp
+            attr.callback "oninput" onChange
             
             "Value" => currentValue
             "Placeholder" => placeholder
