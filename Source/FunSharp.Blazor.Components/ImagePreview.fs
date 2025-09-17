f14721de471490c0fb9ad95e565bc833ff8e6be5
Malbios <no@mail.none>
2025-09-17 23:16:21 +0200

    per-item busy state for prompts (other types still pending)

--- Source/FunSharp.Blazor.Components/ImagePreview.fs
+++ Source/FunSharp.Blazor.Components/ImagePreview.fs
@@ -11,13 +11,28 @@ type ImagePreview() =
     [<Parameter>]
     member val Image : Uri option = None with get, set
     
+    [<Parameter>]
+    member val Clickable = false with get, set
+    
     override this.Render() =
+        
+        let image url =
+            img {
+                attr.style "max-width: 200px; max-height: 200px;"
+                attr.src url
+            }
+        
         div {
             match this.Image with
             | None -> LoadingWidget.render ()
             | Some url ->
-                img {
-                    attr.style "max-width: 200px; max-height: 200px;"
-                    attr.src (url.ToString())
-                }
+                if this.Clickable then
+                    a {
+                        attr.href (url.ToString())
+                        attr.target "_blank"
+                        
+                        url.ToString() |> image
+                    }
+                else
+                    url.ToString() |> image
         }
