244216246b2031de32996cfaa0b8d2df1823ed3b
Malbios <no@mail.none>
2025-09-24 13:14:33 +0200

    new dialog for inspiration 2 prompt workflow

--- Source/FunSharp.Blazor.Components/Button.fs
+++ Source/FunSharp.Blazor.Components/Button.fs
@@ -1,19 +1,27 @@
 ﻿namespace FunSharp.Blazor.Components
 
+open System.Threading.Tasks
 open Bolero.Html
-open Microsoft.AspNetCore.Components
 open Microsoft.AspNetCore.Components.Web
 open Radzen.Blazor
 
 [<RequireQualifiedAccess>]
 module Button =
     
-    let render parent (onClick: unit -> unit) (disabled: bool) label =
-        
-        let onClick = fun (_: MouseEventArgs) -> onClick ()
+    let render (label: string) onClick (disabled: bool) =
         
         comp<RadzenButton> {
             "Text" => label
             "Disabled" => disabled
-            "Click" => EventCallback.Factory.Create<MouseEventArgs>(parent, onClick)
+            
+            attr.callback "Click" (fun (_: MouseEventArgs) -> onClick())
+        }
+        
+    let renderAsync (label: string) (onClick: unit -> Task) (disabled: bool) =
+        
+        comp<RadzenButton> {
+            "Text" => label
+            "Disabled" => disabled
+            
+            attr.task.callback "Click" (fun (_: MouseEventArgs) -> onClick())
         }
