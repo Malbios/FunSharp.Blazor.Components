c4b384edbfdf14cb2279a79a8bd00cde3d5beb68
Malbios <no@mail.none>
2025-09-30 14:40:53 +0200

    cleanup and styling

--- Source/FunSharp.Blazor.Components/Pager.fs
+++ Source/FunSharp.Blazor.Components/Pager.fs
@@ -12,19 +12,25 @@ module Pager =
         let currentPage = (offset / limit)
         let lastPage = (total / limit)
         
-        printfn $"currentPage: {currentPage} lastPage: {lastPage}"
-        
         comp<RadzenStack> {
             "Orientation" => Orientation.Horizontal
             "Wrap" => FlexWrap.Wrap
             "Gap" => "0.1rem"
             
             Button.render "|<" (fun () -> onPageChanged 0) (currentPage = 0)
-            Button.render "<" (fun () -> onPageChanged <| currentPage - 1) (currentPage = 0)
+            
+            div {
+                attr.style "margin-right: 1rem;"
+                Button.render "<" (fun () -> onPageChanged <| currentPage - 1) (currentPage = 0)
+            }
             
             for i in [0..total / limit] do
                 Button.render $"{i + 1}" (fun () -> onPageChanged i) (currentPage = i)
             
-            Button.render ">" (fun () -> onPageChanged <| currentPage + 1) (currentPage = lastPage)
+            div {
+                attr.style "margin-left: 1rem;"
+                Button.render ">" (fun () -> onPageChanged <| currentPage + 1) (currentPage = lastPage)
+            }
+            
             Button.render ">|" (fun () -> onPageChanged lastPage) (currentPage = lastPage)
         }
